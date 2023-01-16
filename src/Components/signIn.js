34a119b0711e6d3be0fbd5dbf3a3a6ca7343cfd6
Muhammad <muhammadkhizer840@gmail.com>
2023-01-16 23:02:06 +0500

    Create pop up for login error

--- src/Components/signIn.js
+++ src/Components/signIn.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-
+import { ToastContainer, toast } from 'react-toastify';
 export default function SignIn({username,password,nav,setPassword,setUsername,
   userData,setUserData,
 }) {
   
-  
+  const [error,setError]=useState(false);
+
   const handleSignIn= async (e)=>{
     e.preventDefault();
     
     const res = await axios.post('https://sdok7nl5h2.execute-api.ap-northeast-1.amazonaws.com/prod/logininstructor',{
       username,
       password
+    }).catch(error=>{
+      showErrorMessage(error.message);
     })
     setUsername('');
     setPassword('');
@@ -20,10 +23,16 @@ export default function SignIn({username,password,nav,setPassword,setUsername,
     await setUserData(data);
     // // console.log(data.user.name)
 
-    // console.log(userData);
+     // console.log(userData);
 
     nav('/courses');
   }
+  
+const showErrorMessage = (error) => {
+  toast.error("Invalid username or password", {
+      position: toast.POSITION.TOP_RIGHT
+  });
+};
   // console.log(username,password)
   return (
     <div className='main'>
@@ -31,16 +40,16 @@ export default function SignIn({username,password,nav,setPassword,setUsername,
         <h1 className='heading'>Sign In</h1>
         <form className='signin-form' onSubmit={handleSignIn}>
 
-          <div className='email-container'>
+          <div className='email-container' >
             <input type="email" placeholder='Username or email' 
               onChange={(e=>{setUsername(e.target.value)})}
-              value={username} />
+              value={username} required/>
           </div>
 
           <div className='password-container'>
             <input type="password" placeholder='Password' 
              onChange={(e=>{setPassword(e.target.value)})}
-             value={password} />
+             value={password} required/>
           </div>
           {/* <div className="checkbox-container">
             <input type="checkbox" />
@@ -48,7 +57,7 @@ export default function SignIn({username,password,nav,setPassword,setUsername,
           </div> */}
           <button className='sign-in-btn btn'>Sign In</button>
         </form>
-        
+          <ToastContainer/>
       </div>
     </div>
   
